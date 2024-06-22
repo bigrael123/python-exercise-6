2bf71836be22c4a2e3cba94d9628628fcd6692dd
bigrael123 <bigrael-x@hotmail.com>
2024-06-21 23:53:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+kilometers = float(input("How many kilometers did the car travel?"))
+days = float(input("How many days did you spend with the car?"))
+price = kilometers * 0.15 + days * 60
+print(f"The kilometers travelled were {kilometers}. You spent {days} days with the car. The final price of the rent is {price}.")
